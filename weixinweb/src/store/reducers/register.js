5e47503b3c6faf3f2e813fec7deb484c5f4d4545
dengly <1350540440@qq.com>
2018-12-04 17:42:05 +0800

    123

--- weixinweb/src/store/reducers/register.js
+++ weixinweb/src/store/reducers/register.js
@@ -1,13 +1,60 @@
 import { handleActions } from 'redux-actions'
-    import { STATUS } from '../types/register'
-    
-    const defaultState = {code:1}
+import { STATUS } from '../types/register'
+import { USER } from '../types/register'
+import { PLACEID } from '../types/register'
+import { DEVICE } from '../types/register'
+    const defaultState = {
+        code:1,
+        user:{},
+        place_id:'',
+        device_detailed:{},
+        flag:[
+            {
+                label:'新增',
+                value:1
+            },
+            {
+                label:'正常',
+                value:9
+            },
+            {
+                label:'欠费',
+                value:10
+            },
+            {
+                label:'断线',
+                value:20
+            },
+            {
+                label:'删除',
+                value:99
+            }
+        ]
+    }
     
     export default handleActions({
         [STATUS]( state , action ){
             return {
               ...state,
               code:action.payload
+            }
+        },
+        [USER]( state , action ){
+            return {
+              ...state,
+              user:action.payload
+            }
+        },
+        [PLACEID]( state , action ){
+            return {
+              ...state,
+              place_id:action.payload
+            }
+        },
+        [DEVICE]( state , action ){
+            return {
+              ...state,
+              device_detailed:action.payload
             }
         },
     },defaultState)
--- weixinweb/src/store/types/register.js
+++ weixinweb/src/store/types/register.js
@@ -1,2 +1,4 @@
 export const STATUS = 'STATUS'
-
+export const USER = 'USER'
+export const PLACEID = 'PLACEID'
+export const DEVICE = 'DEVICE'
