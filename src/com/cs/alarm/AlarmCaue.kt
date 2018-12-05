a22d4e7f611554cdbabe83f91868319dde486e20
lipo <aaaa@QQcom>
2018-12-05 17:40:58 +0800

    first+1

--- src/com/cs/alarm/AlarmCaue.kt
+++ src/com/cs/alarm/AlarmCaue.kt
@@ -2,9 +2,11 @@ package com.cs.alarm
 
 import com.alibaba.fastjson.JSONObject
 import com.cs.ParaType
-import com.cs.common.units.CODE
-import com.cs.common.units.HttpResult
+import com.cs.common.dbHelper.JDBCHelperFactory.helper
+import com.cs.common.units.*
 import com.cs.correctPara
+import com.cs.generateCondition
+import com.cs.sortDeskCondition
 
 /**
  *
@@ -19,30 +21,33 @@ import com.cs.correctPara
 fun queryAlarmCaue(jsonData: JSONObject): HttpResult {
 
 
-	val result: HttpResult = correctPara("id", jsonData, ParaType.LONG)
+	var result: HttpResult = correctPara("user_id", jsonData, ParaType.LONG)
+	result = correctPara("cause_id", jsonData, ParaType.LONG, result)
+	result = correctPara("device_id", jsonData, ParaType.LONG, result)
+	result = correctPara(PAGE_NUMBER, jsonData, ParaType.INT, result)
+	result = correctPara(PAGE_COUNT, jsonData, ParaType.INT, result)
 
 	if (JSONObject.parseObject(result.toString())[CODE] != 0) return result
 
-//	helper.query("select * from alarm where user_id = ${jsonData[ID]}") {
-//		result.addRowToData(
-//			"alarm_list",
-//			it,
-//			arrayOf(
-//				"id",
-//				"user_id",
-//				"device_id",
-//				"cause_id",
-//				"cause_time",
-//				"confirm_time",
-//				"confirm_id",
-//				"create_time",
-//				"note",
-//				"flag"
-//			)
-//		)
-//	}
+	val offset = (jsonData[PAGE_NUMBER] as Int - 1) * jsonData[PAGE_COUNT] as Int
+	result = Success()
+	val where = generateCondition(jsonData)
+	val orderBy = sortDeskCondition(jsonData)
 
-	return result
+	helper.queryBySet("select * from (select  ac.id,ac.name,ac.type,ac.flag,a.create_time,a.cause_id,a.device_id,d.name as device_name from alarm_cause ac inner join alarm a on ac.id = a.cause_id inner join device d on a.device_id = d.id inner join users u on d.user_id = u.id inner join device_list list2 on d.device_list_id = list2.id where a.user_id = ${jsonData["user_id"]}) t where 1 = 1 $where $orderBy limit ${jsonData[PAGE_COUNT]} offset $offset; ") {
+		result.addSetToData(
+			"alarm_list",
+			it,
+			arrayOf(
+				"id", "name", "type", "flag", "create_time", "cause_id", "device_id", "device_name"
+			)
+		)
+	}
+
+	return result.put(
+		"counts",
+		helper.queryWithOneValue("select count(*) from (select  ac.id,ac.name,ac.type,ac.flag,a.create_time,a.cause_id,a.device_id,d.name as device_name from alarm_cause ac inner join alarm a on ac.id = a.cause_id inner join device d on a.device_id = d.id inner join users u on d.user_id = u.id inner join device_list list2 on d.device_list_id = list2.id where a.user_id = ${jsonData["user_id"]}) t where 1 = 1 $where")
+	)
 }
 
 
--- src/com/cs/device/Device.kt
+++ src/com/cs/device/Device.kt
@@ -8,6 +8,7 @@ import com.cs.common.units.*
 import com.cs.correctPara
 import com.cs.entitys.DeviceFactory
 import com.cs.generateCondition
+import com.cs.sortDeskCondition
 
 /**
  *
@@ -23,16 +24,64 @@ import com.cs.generateCondition
 fun queryDevice(jsonData: JSONObject): HttpResult {
 
 	var result: HttpResult = correctPara("user_id", jsonData, ParaType.LONG, isNeed = true)
-	correctPara("place_id", jsonData, ParaType.LONG, result, isNeed = true)
-	correctPara("id", jsonData, ParaType.LONG, result, isNeed = false)
+	result = correctPara("place_id", jsonData, ParaType.LONG, result, isNeed = true)
+	result = correctPara("id", jsonData, ParaType.LONG, result, isNeed = false)
+	result = correctPara(PAGE_NUMBER, jsonData, ParaType.INT, result)
+	result = correctPara(PAGE_COUNT, jsonData, ParaType.INT, result)
+
+	if (JSONObject.parseObject(result.toString())[CODE] != 0) return result
+
+	val offset = (jsonData[PAGE_NUMBER] as Int - 1) * jsonData[PAGE_COUNT] as Int
+	result = Success()
+	val where = generateCondition(jsonData)
+	val orderBy = sortDeskCondition(jsonData)
+
+
+	helper.queryBySet("select * from (select d.id,d.last_connect_time,d.msisdn,u.name as user_id,p.name as place_id,list2.name as device_list_id,d.name,d.create_time,a.name as admin_id,d.note,d.flag from device d inner join administrator a on d.admin_id = a.id inner join users u on d.user_id = u.id inner join place p on d.place_id = p.id inner join device_list list2 on d.device_list_id = list2.id where d.user_id =  ${jsonData["user_id"]} and place_id = ${jsonData["place_id"]} ) t where 1 = 1  $where $orderBy limit ${jsonData[PAGE_COUNT]} offset $offset;") { its ->
+		result.addSetToData(
+			"device_list",
+			its,
+			arrayOf(
+				ID,
+				MSISDN,
+				"user_id",
+				"place_id",
+				"device_list_id",
+				"name",
+				"create_time",
+				"admin_id",
+				"last_connect_time",
+				"note",
+				"flag"
+			)
+		)
+	}
+
+
+	return result.put(
+		"counts",
+		helper.queryWithOneValue("select count(*) from (select d.id,d.last_connect_time,d.msisdn,u.name as user_id,p.name as place_id,list2.name as device_list_id,d.name,d.create_time,a.name as admin_id,d.note,d.flag from device d inner join administrator a on d.admin_id = a.id inner join users u on d.user_id = u.id inner join place p on d.place_id = p.id inner join device_list list2 on d.device_list_id = list2.id where d.user_id =  ${jsonData["user_id"]} and place_id = ${jsonData["place_id"]} ) t where 1 = 1  $where")
+	)
+}
+
+/**
+ * 查询单个设备
+ */
+
+fun queryByDeviceId(jsonData: JSONObject): HttpResult {
+
+	var result: HttpResult = correctPara("user_id", jsonData, ParaType.LONG, isNeed = true)
+	result = correctPara("place_id", jsonData, ParaType.LONG, result, isNeed = true)
+	result = correctPara("id", jsonData, ParaType.LONG, result, isNeed = true)
 
 	if (JSONObject.parseObject(result.toString())[CODE] != 0) return result
 
 	result = Success()
-
 	val where = generateCondition(jsonData)
+	val orderBy = sortDeskCondition(jsonData)
 
-	helper.queryBySet("select * from (select d.id,d.last_connect_time,d.msisdn,u.name as user_id,p.name as place_id,list2.name as device_list_id,d.name,d.create_time,a.name as admin_id,d.note,d.flag from device d inner join administrator a on d.admin_id = a.id inner join users u on d.user_id = u.id inner join place p on d.place_id = p.id inner join device_list list2 on d.device_list_id = list2.id where d.user_id =  ${jsonData["user_id"]} and place_id = ${jsonData["place_id"]} ) t where 1 = 1 $where") { its ->
+
+	helper.queryBySet("select * from (select d.id,d.last_connect_time,d.msisdn,u.name as user_id,p.name as place_id,list2.name as device_list_id,d.name,d.create_time,a.name as admin_id,d.note,d.flag from device d inner join administrator a on d.admin_id = a.id inner join users u on d.user_id = u.id inner join place p on d.place_id = p.id inner join device_list list2 on d.device_list_id = list2.id where d.user_id =  ${jsonData["user_id"]} and place_id = ${jsonData["place_id"]} ) t where 1 = 1 and id = ${jsonData[ID]} $where $orderBy;") { its ->
 		result.addSetToData(
 			"device_list",
 			its,
