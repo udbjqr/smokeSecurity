b6f56e8a58e81954e7af6ce0daf0538a19e500b6
dengly <1350540440@qq.com>
2018-12-05 17:19:34 +0800

    12

--- weixinweb/src/store/types/register.js
+++ weixinweb/src/store/types/register.js
@@ -2,3 +2,7 @@ export const STATUS = 'STATUS'
 export const USER = 'USER'
 export const PLACEID = 'PLACEID'
 export const DEVICE = 'DEVICE'
+export const NUMBERS = 'NUMBERS'
+export const PLACEDETAILID = 'PLACEDETAILID'
+export const MESSAGEID = 'MESSAGEID'
+
